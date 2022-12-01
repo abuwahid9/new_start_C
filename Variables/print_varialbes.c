bcc100986ebd772d3627965a03852701f8f37698
abuwahid9 <abuwahid9.gmail.com>
2022-12-01 14:53:25 +0600

    2-update

---
+++ Variables/print_varialbes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+int x = 15;
+int m = 15;
+float y= 15.5;
+char l = 'a';
+printf ("%d\n" , x);
+printf ("%f\n" , y);
+printf ("%c\n" , l);
+printf ("\n");
+printf ("print the number : %d" , x);
+printf (" print the number : %d \n print the character : %c" , x , l);
+
+
+
+int sum = x + m;
+printf (" \n Print the sum : ");
+printf ("%d\n" , sum);
+
+
+  return 0;
+}
