9d9d74634716e3716aba6b615b47605a4bdb4010
abuwahid9 <abuwahid9.gmail.com>
2022-12-04 23:44:12 +0600

    update-2

---
+++ Oparetors/increment/decrement.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int x=10;
+//int b=20;
+int z=15;
+//int a=25;
+printf("%d",++x);
+//printf("\n%d",b++);
+printf("\n%d",--z);
+//printf("\n%d",a--);
+return 0;
+}
---
+++ Oparetors/print_oparetor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+int x = 15;
+int y = 15;
+int z = x + y;
+int a = x - y;
+int b = x * y;
+int c = x / y;
+int d = x % y;
+printf("Print the Addition : %d\n", z);
+printf("Print the Subtraction : %d\n", a);
+printf("Print the Division : %d\n", b);
+printf("Print the Multiplication : %d\n", c);
+printf("Print the Modulus : %d\n", d);
+}
